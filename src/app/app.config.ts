161b2d8669f4ec0ff09bbc163b71a3acfdf0978a
Nahuel Nina <ninacristian55@gmail.com>
2025-12-15 22:31:58 -0400

    correct config and routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAngularSvgIcon(),
   ]
 };
